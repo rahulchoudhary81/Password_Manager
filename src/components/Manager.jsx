ec87732414021798ee369f6cfe45a40593d584b8
rahulchoudhary81 <rahulchoudhary81000@gmail.com>
2025-06-29 23:44:43 +0530

    .

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -5,15 +5,22 @@ const Manager = () => {
         <>
             <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
 
-            <div className="mx-auto bg-slate-800 max-w-4xl">
-                <h1>PassOp</h1>
+            <div className="bg-slate-50 mycontainer">
+                <h1 className='text-4xl text font-bold text-center'>
+                    <span className='text-green-700'>&lt;</span>
+                    <span>Pass</span><span className='text-green-700'>OP</span>
+                    <span className='text-green-700'>&gt;</span>
 
-                <div className='text-white flex flex-col p-4'>
-                    <input className='rounded-full' type="text" name='' id='' />
-                    <div className='flex'>
-                        <input type="text" />
-                        <input type="text" />
+                </h1>
+                <p className='text-green-900 text-lg text-center'>Your own Password Manager</p>
+
+                <div className='flex flex-col py-4 px-40 text-black gap-8'>
+                    <input className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name='' id='' />
+                    <div className='flex w-full justify-between gap-8'>
+                        <input className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name='' id='' />
+                        <input className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name='' id='' />
                     </div>
+                    <button>Add Password</button>
                 </div>
             </div>
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='bg-purple-200 flex justify-between items-center px-4 h-14'>
-            <div className="logo font-bold">PassOp</div>
-            <ul>
-                <li className='flex gap-4'>
-                    <a className='hover:font-bold' href="#">About</a>
-                    <a className='hover:font-bold' href="/">Home</a>
-                    <a className='hover:font-bold' href="#">Contact</a>
-                </li>
-            </ul>
+        <nav className='bg-slate-800 text-white'>
+            <div className="mycontainer flex justify-between items-center px-44 py-5 h-14">
+
+                <div className="logo font-bold text-white text-2xl">
+                    <span className='text-green-700'>&lt;</span>
+                    <span>Pass</span><span className='text-green-700'>OP</span>
+                    <span className='text-green-700'>&gt;</span>
+                </div>
+                <ul>
+                    <li className='flex gap-4'>
+                        <a className='hover:font-bold' href="/">Home</a>
+                        <a className='hover:font-bold' href="#">About</a>
+                        <a className='hover:font-bold' href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
